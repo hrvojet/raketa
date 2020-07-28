06cec8788fab379ab8a521eeaaabb17ed4361575
Hrvoje T <hrvojetrcak@gmail.com>
2020-07-28 23:00:23 +0200

    Functional rocket with one direction thrust

---
+++ Assets/Scenes/Rocket.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    void ProcessInput()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+        }
+    }
+}
