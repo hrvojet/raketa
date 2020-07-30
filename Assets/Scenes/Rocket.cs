9d82ddf69378d643dcd43eaaa34f864cacc4b3d0
Hrvoje T <hrvojetrcak@gmail.com>
2020-07-30 18:33:23 +0200

    Created level (added obsticles, background, finish-pad)

--- Assets/Scenes/Rocket.cs
+++ Assets/Scenes/Rocket.cs
@@ -6,51 +6,76 @@ public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
     AudioSource rocketSound;
+    [SerializeField]float mainThrust = 50f;
+    [SerializeField]float rcsThrust = 200f;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
         rocketSound = GetComponent<AudioSource>();
+
+        rocketSound.loop = true;
+        rocketSound.Play();
     }
 
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        Thrust();
+        Rotate();
     }
 
-    void ProcessInput()
+    void OnCollisionEnter(Collision collision)
     {
-        if(Input.GetKey(KeyCode.Space))
+        switch (collision.gameObject.tag)
         {
-            rigidBody.AddRelativeForce(Vector3.up);
-            if(!rocketSound.isPlaying)
+            case "Friendly":
+                print("Friendly");
+                break;
+            case "Fuel":
+                print("Fuel");
+                break;
+            default:
+                print("Dead");
+                // kill player
+                break;
+        }
+    }
+
+    private void Thrust()
+    {        
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+            rocketSound.volume = 1f;
+            if (!rocketSound.isPlaying)
             {
-                rocketSound.Play();
+                //rocketSound.Play();
             }
         }
         else
         {
-            rocketSound.Stop();
+            rocketSound.volume = 0f;
+            //rocketSound.Stop();
         }
+    }
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true; // take manual control of rotation
         
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
+            
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
         }
-        /*
-        else if (Input.GetKey(KeyCode.W))
-        {
-            transform.Rotate(Vector3.right);
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            transform.Rotate(-Vector3.right);
-        }*/
+
+        rigidBody.freezeRotation = false; // resume physics control rotation
     }
 }
