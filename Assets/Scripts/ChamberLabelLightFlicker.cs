b464e479feb6dce490cc37851742208b6871157e
Hrvoje <hrvojetrcak@gmail.com>
2022-02-12 18:58:15 +0100

    Add Morse code light flicker on level 4
    On level 4, one chamber indicator light flickers 'UP' in
    Morse code to indicate hidden chamber

--- Assets/Scripts/ChamberLabelLightFlicker.cs
+++ Assets/Scripts/ChamberLabelLightFlicker.cs
@@ -1,18 +1,82 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class ChamberLabelLightFlicker : MonoBehaviour
 {
+    private Light _light;
+    
     // Start is called before the first frame update
     void Start()
     {
+        _light = GetComponent<Light>();
+        TurnLightOff();
         
+        // InvokeRepeating("ToggleLight", 0.5f, 0.5f);
+        StartCoroutine(PlayShort());
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void ToggleLight()
     {
+        _light.enabled = !_light.enabled;
+    }
+
+    private void TurnLightOn()
+    {
+        _light.enabled = true;
+    }
+
+    private void TurnLightOff()
+    {
+        _light.enabled = false;
+    }
+
+    /*
+     * U = .._ (short, short, long)
+     * P = .__. (short, long, long, short)
+     * Pause between letters = _
+     */
+    
+    // Odvratan hardkodiran moreseov kod za UP
+    IEnumerator PlayShort()
+    {
+        // start with light turned off
+        yield return new WaitForSeconds(1.5f);
         
+        while (true)
+        {
+            TurnLightOn();
+            yield return new WaitForSeconds(0.5f);
+            TurnLightOff();
+            yield return new WaitForSeconds(0.5f);
+            TurnLightOn();
+            yield return new WaitForSeconds(0.5f);
+            TurnLightOff();
+            yield return new WaitForSeconds(0.5f);
+            TurnLightOn();
+            yield return new WaitForSeconds(1.5f);
+            
+            TurnLightOff();
+            yield return new WaitForSeconds(1.5f);
+
+            TurnLightOn();
+            yield return new WaitForSeconds(0.5f);
+            TurnLightOff();
+            yield return new WaitForSeconds(0.5f);
+            TurnLightOn();
+            yield return new WaitForSeconds(1.5f);
+            TurnLightOff();
+            yield return new WaitForSeconds(0.5f);
+            TurnLightOn();
+            yield return new WaitForSeconds(1.5f);
+            TurnLightOff();
+            yield return new WaitForSeconds(0.5f);
+            TurnLightOn();
+            yield return new WaitForSeconds(0.5f);
+            TurnLightOff();
+            yield return new WaitForSeconds(1.5f);
+        }
     }
 }
