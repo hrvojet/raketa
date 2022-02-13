b93a97731b573eef28a5ed08a251f0232d78ee0f
Hrvoje <hrvojetrcak@gmail.com>
2022-02-13 01:53:12 +0100

    Show final time on ending screen

--- Assets/Scripts/EndingScript.cs
+++ Assets/Scripts/EndingScript.cs
@@ -1,21 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using RocketGameLevelManager;
+using UnityEngine.UI;
+
 public class EndingScript : MonoBehaviour
 {
 
     private float _finalTime;
+    public Text time;
+    
     // Start is called before the first frame update
     void Start()
     {
-        _finalTime = GameLoader.GetPreservedTime();
-        Debug.Log("Final time: " + (_finalTime - 2f));
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        _finalTime = GameLoader.GetPreservedTime() - 2;
+        TimeSpan timeSpan = TimeSpan.FromSeconds(_finalTime);
+        time.text = timeSpan.ToString(@"mm\:ss\:fff");
+        Debug.Log("Final time: " + _finalTime);
     }
 }
